c51e1aa3dd62b7eaa80225b8a8851ca57cc33c38
SirajArf <siraj.arfeen1997@gmail.com>
2022-02-25 20:31:45 +0530

    added session storage

--- index.js
+++ index.js
@@ -1,18 +1,20 @@
-const express = require('express');
-const morgan = require('morgan');
-const routes = require('./routes/index');
-const connectDB = require('./config/db');
+const express = require("express");
+const morgan = require("morgan");
+const mongoose = require("mongoose");
+const routes = require("./routes/index");
+const connectDB = require("./config/db");
 //const { engine } = require('express-handlebars');
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const passport = require("passport");
-const session = require('express-session');
-const path = require('path');
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session);
+const path = require("path");
 
 //.env config
-require("dotenv").config({path:'./config/config.env'});
+require("dotenv").config({ path: "./config/config.env" });
 
 //passport config
-require('./config/passport')(passport)
+require("./config/passport")(passport);
 
 connectDB();
 
@@ -24,37 +26,36 @@ const app = express();
 
 //logging
 
-if(process.env.NODE_ENV === 'development'){
-    app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 //handlebars
-app.engine('.hbs', exphbs.engine({ defaultLayout: 'main', extname: '.hbs'}));
-app.set('view engine', '.hbs');
+app.engine(".hbs", exphbs.engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", ".hbs");
 
 //session middleware
-app.use(session({
-    secret: 'passport',
+app.use(
+  session({
+    secret: "passport",
     resave: false,
     saveUninitialized: false,
-}))
-
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  })
+);
 
 //passport middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-
 //Static folder
- app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 //routes
 
- app.use('/', routes)
- app.use('/auth', require('./routes/auth'))
+app.use("/", routes);
+app.use("/auth", require("./routes/auth"));
 
-
-
-app.listen(port,(req, res) => {
-    console.log(`server is in ${process.env.NODE_ENV} running at ${port}`)
-})
+app.listen(port, (req, res) => {
+  console.log(`server is in ${process.env.NODE_ENV} running at ${port}`);
+});
