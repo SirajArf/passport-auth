ae9874dc9d0b29d04be700c83edbb4121f188cfd
SirajArf <siraj.arfeen1997@gmail.com>
2022-02-26 11:04:19 +0530

    added edit feature

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const exphbs = require("express-handlebars");
 const passport = require("passport");
 const session = require("express-session");
 const MongoStore = require("connect-mongodb-session")(session);
+const methodOverride = require('method-override');
 const path = require("path");
 
 //.env config
@@ -16,14 +17,28 @@ require("dotenv").config({ path: "./config/config.env" });
 //passport config
 require("./config/passport")(passport);
 
+//database connect
 connectDB();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+//body-parser
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+//method override
+app.use(methodOverride(function (req, res) {
+  if(req.body && typeof req.body === 'object' && '_method' in req.body) {
+
+    let method = req.body._method
+    delete req.body._method
+    return method
+  }
+}))
+
 
 //logging
 
@@ -31,16 +46,19 @@ if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
 }
 
-const { formatDate, stripTags, truncate, editIcon } = require("./helpers/helpers");
+const {
+  formatDate,
+  stripTags,
+  truncate,
+  editIcon,
+  select,
+} = require("./helpers/helpers");
 
 //handlebars
 app.engine(
   ".hbs",
   exphbs.engine({
-    helpers: { formatDate,
-                stripTags,
-                truncate,
-                editIcon },
+    helpers: { formatDate, stripTags, truncate, editIcon, select },
     defaultLayout: "main",
     extname: ".hbs",
   })
@@ -62,9 +80,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 app.use(function (req, res, next) {
-    res.locals.user = req.user || null
-    next()
-})
+  res.locals.user = req.user || null;
+  next();
+});
 
 //Static folder
 app.use(express.static(path.join(__dirname, "public")));
--- routes/stories.js
+++ routes/stories.js
@@ -19,22 +19,67 @@ router.post("/", ensureAuth, async (req, res) => {
   }
 });
 
-router.get('/', ensureAuth, async (req, res) => {
+router.get("/", ensureAuth, async (req, res) => {
   try {
-    const stories = await Story.find({ status: 'public' })
-      .populate('user')
-      .sort({ createdAt: 'desc' })
-      .lean()
+    const stories = await Story.find({ status: "public" })
+      .populate("user")
+      .sort({ createdAt: "desc" })
+      .lean();
 
-    res.render('stories/index', {
-      stories, 
-    })
+    res.render("stories/index", {
+      stories,
+    });
+  } catch (err) {
+    console.error(err);
+    res.render("error/serverError");
+  }
+});
+
+router.get("/edit/:id", ensureAuth, async (req, res) => {
+  try {
+    const story = await Story.findOne({
+      _id: req.params.id,
+    }).lean();
+
+    if (!story) {
+      return res.render("error/notFound");
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect("/stories");
+    } else {
+      res.render("stories/edit", {
+        story,
+      });
+    }
+  } catch (err) {
+    console.error(err);
+    return res.render("error/500");
+  }
+});
+
+router.put('/:id', ensureAuth, async (req, res) => {
+  try {
+    let story = await Story.findById(req.params.id).lean()
+
+    if (!story) {
+      return res.render('error/notFound')
+    }
+
+    if (story.user != req.user.id) {
+      res.redirect('/stories')
+    } else {
+      story = await Story.findOneAndUpdate({ _id: req.params.id }, req.body, {
+        new: true,
+        runValidators: true,
+      })
+
+      res.redirect('/dashboard')
+    }
   } catch (err) {
     console.error(err)
-    res.render('error/serverError')
+    return res.render('error/serverError')
   }
 })
-
-
 
 module.exports = router;
